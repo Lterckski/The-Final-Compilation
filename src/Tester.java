73623e4461dc2fc08b6afa6c1f89bd87f5284738
ZyrilR <zyrilrestauro@gmail.com>
2025-09-27 22:41:41 +0800

    Implement Character class & added initial stats to playable characters

---
+++ src/Tester.java
@@ -0,0 +1,15 @@
+import characters.Kael;
+import characters.Karl;
+import characters.Simon;
+
+public class Tester {
+    public static void main(String[] args){
+        Kael kael = new Kael();
+        Karl karl = new Karl();
+        Simon simon = new Simon();
+
+        kael.showStats();
+        karl.showStats();
+        simon.showStats();
+    }
+}
---
+++ src/characters/Kael.java
@@ -0,0 +1,7 @@
+package characters;
+
+public class Kael extends  Character{
+    public Kael() {
+        super("Kael Saint Laurent", 100, 10, 60, 15, 20);
+    }
+}
