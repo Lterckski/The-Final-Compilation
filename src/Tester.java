4086d4dbe2d734eccdfe015f5427ea82b984a9cd
ZyrilR <zyrilrestauro@gmail.com>
2025-09-28 11:02:26 +0800

    Added-randomUtil-class-Added-basic-combat-logic-Implemented-Kael's-skills

--- src/Tester.java
+++ src/Tester.java
@@ -1,15 +1,30 @@
 import characters.Kael;
 import characters.Karl;
 import characters.Simon;
+import characters.testEnemy;
 
 public class Tester {
     public static void main(String[] args){
         Kael kael = new Kael();
-        Karl karl = new Karl();
-        Simon simon = new Simon();
+        testEnemy enemy = new testEnemy();
 
         kael.showStats();
-        karl.showStats();
-        simon.showStats();
+        enemy.showStats();
+
+        kael.bladeRush(enemy);
+        System.out.println();
+        kael.guardBreaker(enemy);
+        System.out.println();
+        kael.eternalCrossSlash(enemy);
+        System.out.println();
+
+        kael.showStats();
+        enemy.showStats();
+
+        System.out.println();
+        kael.eternalCrossSlash(enemy);
+        System.out.println();
+
+        kael.showStats();
     }
 }
