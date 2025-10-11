113220f6d4774799ab0b0c4e04f46c7a589a6dd2
Simon <belinomarcel@gmail.com>
2025-10-11 23:23:08 +0800

    I implemented world 3 enemies as well as world 3 miniboss :)

---
+++ src/enemies/Enemy1World3.java
@@ -0,0 +1,35 @@
+package enemies;
+
+import characters.Character;
+import utils.RandomUtil;
+
+public class Enemy1World3 extends Enemy {
+
+    public Enemy1World3() { super("Flame Revenant", 982, 44, 57); }
+
+    public void emberBurst(Character target) {
+        System.out.println("🔥 " + name + " unleashes Ember Burst!");
+        if(target.getEffects().checkDodge()) return;
+
+        int damage = (int)RandomUtil.range(attack * 1.00, attack * 1.35);
+        int reduced = damage - target.getDefense();
+        if (reduced < 0) reduced = 0;
+
+        System.out.println("→ Ember Burst hits for " + reduced + " damage!");
+        target.takeDamage(reduced);
+        target.getEffects().applyBurn(2);
+    }
+
+    @Override
+    public void showSkills() {
+        System.out.println("\n------- FLAME REVENANT SKILLS -------");
+        System.out.println("Description: Flame Revenants hurl blazing fireballs from afar, burning anything in it's path.");
+        System.out.println("Damage: (" + (int)(attack * 1.00) + " — " + (int)(attack * 1.35) + ")");
+        System.out.println("Effects:");
+        System.out.println("- Applies Burn for 2 turns");
+        System.out.println("------------------------------------");
+    }
+
+    @Override
+    public void turn(Character target) { emberBurst(target); }
+}
---
+++ src/enemies/Enemy4World3.java
@@ -0,0 +1,39 @@
+package enemies;
+
+import characters.Character;
+import utils.RandomUtil;
+
+public class Enemy4World3 extends Enemy {
+
+    public Enemy4World3() { super("Soulflayer Gargoyle", 1493, 58, 46); }
+
+    public void soulScream(Character target) {
+        System.out.println("😈 " + name + " lets out a deafening Soul Scream!");
+        if(target.getEffects().checkDodge()) return;
+
+        int damage = (int)RandomUtil.range(attack * 1.00, attack * 1.20);
+        int reduced = damage - target.getDefense();
+        if(reduced < 0) reduced = 0;
+
+        System.out.println("→ Soul Scream hits for " + reduced + " damage!");
+        target.takeDamage(reduced);
+
+        if(RandomUtil.chance(30)) {
+            System.out.println("💫 " +  target.getName() + " is confused!");
+            target.getEffects().applyConfuse();
+        }
+    }
+
+    @Override
+    public void showSkills() {
+        System.out.println("\n------- SOULFLAYER GARGOYLE SKILLS -------");
+        System.out.println("Description: Soulflayer Gargoyle unleashes a soul crushing scream, confusing it's victim.");
+        System.out.println("Damage: (" + (int)(attack * 1.00) + " — " + (int)(attack * 1.20) + ")");
+        System.out.println("Effects:");
+        System.out.println("- Chance to apply Confused to target");
+        System.out.println("------------------------------------");
+    }
+
+    @Override
+    public void turn(Character target) { soulScream(target); }
+}
